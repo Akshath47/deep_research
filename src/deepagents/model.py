6a4d52b6696d3e4a0d2dd4d43d7aeddc3f98d196
Akshath47 <akshathyennam@gmail.com>
2025-09-23 19:28:21 +0530

    Changed deep agents modelt open ai

--- src/deepagents/model.py
+++ src/deepagents/model.py
@@ -1,5 +1,10 @@
-from langchain_anthropic import ChatAnthropic
+# from langchain_anthropic import ChatAnthropic
 
+
+# def get_default_model():
+#     return ChatAnthropic(model_name="claude-sonnet-4-20250514", max_tokens=64000)
+
+from langchain_openai import ChatOpenAI
 
 def get_default_model():
-    return ChatAnthropic(model_name="claude-sonnet-4-20250514", max_tokens=64000)
+    return ChatOpenAI(model="gpt-4o", temperature=0)
